42d9d7a9f4d8dab0a02ca7c265d0c75b2544ef01
citli <citlima29@gmail.com>
2026-07-20 02:10:26 +0100

    changing for being accessible

--- mechanic-shop/src/api/axios.js
+++ mechanic-shop/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    //baseURL: "http://192.168.1.73:8000/api",
-    baseURL: "http://localhost:8000/api",
+    baseURL: "http://192.168.1.73:8000/api",
+    //baseURL: "http://localhost:8000/api",
     headers: {
         "Content-Type": "application/json"
     }
